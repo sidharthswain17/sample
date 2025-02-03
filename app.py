ca83aee33cf1016487e4e99432a38e680bb1d345
Siharth Swain <getsidgo07@gmail.com>
2025-02-03 21:24:35 +0530

    qualities added

--- app.py
+++ app.py
@@ -6,4 +6,7 @@ col1,col2 =st.columns(2)
 with col1:
     st.image(r"chat1.jpg")
 with col2:
-    st.title("A sister is both a friend and a protector, always there to share laughter, offer comfort, and create lifelong memories. ")    
+    st.title("A sister is both a friend and a protector, always there to share laughter, offer comfort, and create lifelong memories. ") 
+st.title("qualities")
+st.subheader("murkha")       
+st.subheader("moti")       
