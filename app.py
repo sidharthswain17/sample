80489dff913b4d6933b6efa2d7fcec3551a0dd34
Siharth Swain <getsidgo07@gmail.com>
2025-02-03 20:16:51 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("gundiiieeeee")
+col1,col2 =st.columns(2)
+
+with col1:
+    st.image(r"chat1.jpg")
+with col2:
+    st.title("A sister is both a friend and a protector, always there to share laughter, offer comfort, and create lifelong memories. ")    
